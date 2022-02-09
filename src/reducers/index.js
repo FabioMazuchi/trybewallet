206d1f6bf7fa117c5762047d01f825e7c8298267
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-02-09 15:19:24 -0300

    configura store e reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,14 @@
-// import user from './user';
-// import wallet from './wallet';
+import userReducer from './user';
+import walletReducer from './wallet';
+
+const { combineReducers } = require('redux');
 
 // Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves 'user' e 'wallet' no seu estado global
+
+const rootReducer = combineReducers({
+  userReducer,
+  walletReducer,
+});
+
+export default rootReducer;
