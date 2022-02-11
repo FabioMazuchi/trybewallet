ede2f4d132861670bda7257249611d5f073609e7
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-02-11 13:21:32 -0300

    ajusta req 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,45 +1,40 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-// import Rows from "./Rows";
 
 class Table extends Component {
   render() {
     const { despesas, isLoading } = this.props;
-    if (isLoading) return 'Loading';
+    // if (isLoading) return "Loading";
     return (
-      <div>
-        {despesas !== undefined && (
-          <table>
-            <tr>
-              <td>Descrição</td>
-              <td>Tag</td>
-              <td>Método de pagamento</td>
-              <td>Valor</td>
-              <td>Moeda</td>
-              <td>Câmbio utilizado</td>
-              <td>Valor convertido</td>
-              <td>Moeda de conversão</td>
-              <td>Editar/Excluir</td>
-            </tr>
-            {despesas.map((despesa) => (
-              <tr key={despesa.id}>
-                <td>{despesa.description}</td>
-                <td>{despesa.tag}</td>
-                <td>{despesa.currency}</td>
-                <td>{despesa.value}</td>
-                <td>{despesa.exchangeRates[despesa.moeda].name}</td>
-                <td>{despesa.exchangeRates[despesa.moeda].ask}</td>
-                <td>
-                  {(
-                    despesa.exchangeRates[despesa.moeda].ask * despesa.value
-                  ).toFixed(2)}
-                </td>
-                <td>{despesa.exchangeRates[despesa.moeda].name}</td>
-              </tr>
-            ))}
-          </table>
-        )}
-      </div>
+      <table>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+        {despesas.map((despesa) => (
+          <tr key={despesa.id}>
+            <td>{despesa.description}</td>
+            <td>{despesa.tag}</td>
+            <td>{despesa.currency}</td>
+            <td>{despesa.value}</td>
+            <td>{despesa.exchangeRates[despesa.currency].name}</td>
+            <td>{despesa.exchangeRates[despesa.currency].ask}</td>
+            <td>
+              {(
+                despesa.exchangeRates[despesa.currency].ask * despesa.value
+              ).toFixed(2)}
+            </td>
+            <td>{despesa.exchangeRates[despesa.currency].name}</td>
+          </tr>
+        ))}
+      </table>
     );
   }
 }
