9ab5d1ae7b8a7af6c8b906aa5a5b6062b663e646
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-01 08:33:43 -0300

    Updates

---
+++ precache-manifest.f321b9ef447f12edd1733ed15a9439f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5737b21788ff3a493caf6b29f61e37c1",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "f7167b918debf59738fd",
+    "url": "/trybewallet/static/css/main.e5d69484.chunk.css"
+  },
+  {
+    "revision": "68cb0e8712c530615ab4",
+    "url": "/trybewallet/static/js/2.bd8b1eba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.bd8b1eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7167b918debf59738fd",
+    "url": "/trybewallet/static/js/main.034b9d0b.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  },
+  {
+    "revision": "ccadf54377a8d01549dec31bcf771dde",
+    "url": "/trybewallet/static/media/github.ccadf543.png"
+  },
+  {
+    "revision": "5bb3ed2baaad92f1a4293abcd4b5e1eb",
+    "url": "/trybewallet/static/media/linkedin.5bb3ed2b.png"
+  },
+  {
+    "revision": "c3ab55385ee1be8f985165c87f3b1a9b",
+    "url": "/trybewallet/static/media/wallet.c3ab5538.png"
+  }
+]);
